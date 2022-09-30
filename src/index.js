0d7a8ee7f91c637c117b2cbd069249d2ef4b72d1
AVSCED <anantvikramshukla2022sep@cedcoss.com>
2022-09-30 17:58:50 +0530

    Encryption

---
+++ src/index.js
@@ -0,0 +1,24 @@
+function submit()
+{
+    let enc =[];
+    let ap=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let a= document.getElementById("encrypt").value;
+    let b=[];
+    let c=ap.length;
+    for (i=0;i<a.length;i++){
+        b[i]=a.charAt(i);
+        console.log(b[i]);
+        for(j=0;j<c;j++)
+        {
+            if (b[i]== ap[j])
+            {
+                b[i]=ap[j+1];
+                enc[i]=ap[j+1];
+                console.log(enc[i]);
+            }
+        }
+        
+    }
+document.getElementById("encrypted").innerHTML=enc;
+    console.log(enc);
+}
